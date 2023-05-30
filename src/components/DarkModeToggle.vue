507fc14a381b61a64098650e1b0e9d53ee4e7ca8
Brandon S <atomicbcs@gmail.com>
2023-05-29 23:07:35 -0400

    dark mode

---
+++ src/components/DarkModeToggle.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <b-switch v-model="darkMode" :class="{ 'has-text-white': darkMode }">Dark Mode</b-switch>
+    </div>
+</template>
+  
+<script>
+export default {
+    data() {
+        return {
+            darkMode: false
+        }
+    },
+    mounted() {
+        // Retrieve dark mode state from localStorage on component mount
+        this.darkMode = localStorage.getItem('darkMode') === 'true'
+    },
+    watch: {
+        darkMode(newVal) {
+            // Method to be triggered when the switch is toggled
+            this.$emit('dark-mode-toggle', this.darkMode)
+        }
+    },
+}
+</script>
+  
